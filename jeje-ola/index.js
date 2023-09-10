4ad603ea5419f08ba5d064f2e0c04e5ec82a1919
prex12 <allihuzzain@gmail.com>
2023-09-10 09:41:12 +0100

    fix bugs

--- jeje-ola/index.js
+++ jeje-ola/index.js
@@ -1,21 +1,29 @@
 // var arr = $(".img-property");
 // var arrLength = arr.length;
 // for (var i = 0; i < arrLength; i++)
-    $(".img-property").on("click", ()=>{
-        var buttonPressed = $(".img-property").attr("id");
-        selectImage(buttonPressed);
-    })
+
+var images = document.querySelectorAll(".img-property").length;
+    for (var i = 0; i < images; i++){
+        document.querySelectorAll(".img-property")[i].addEventListener("click", ()=>{
+            var buttonPressed = this;
+            selectImage(buttonPressed);
+        })
+    }
+        
 
     const selectImage= (welp)=>{
         switch (welp){
-            case 'one': $(".img-big").attr("src", "./imgs/Jeje-ola interiors/3d-rendering-modern-dining-room-living-room-with-luxury-decor-green-sofa.jpg")
-            break;
+            case "one": 
+                document.querySelector(".img-big").setAttribute("src", "./imgs/Jeje-ola interiors/3d-rendering-modern-dining-room-living-room-with-luxury-decor-green-sofa.jpg")
+                break;
 
-            case 'two': $(".img-big").attr("src", "./imgs/Jeje-ola interiors/3d-rendering-white-wood-living-room-near-bedroom-upstair.jpg")
-            break;
+            case 'two': 
+                document.querySelector(".img-big").setAttribute("src", "./imgs/Jeje-ola interiors/3d-rendering-white-wood-living-room-near-bedroom-upstair.jpg")
+                break;
 
-            case 'three': $(".img-big").attr("src", "./imgs/Jeje-ola interiors/modern-luxury-bedroom-suite-bathroom.jpg")
-            break;
+            case 'three': 
+                document.querySelector(".img-big").setAttribute("src", "./imgs/Jeje-ola interiors/modern-luxury-bedroom-suite-bathroom.jpg")
+                break;
 
             default:
                 console.log("unexpected input");
--- jeje-ola/server.js
+++ jeje-ola/server.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
 
-// app.use(bodyParser.urlencoded("extended : true"));
+app.use(bodyParser.urlencoded("extended : true"));
+app.use(express.static('public'));
+
 app.get("/",function(req, res){
     res.sendFile(__dirname + "/index.html")
 })
+app.get("/aboutUs.html", (req, res) =>{
+    res.sendFile(__dirname +"/aboutUs.html");
+})
+
+app.get("/portfolio.html", (req, res) =>{
+    res.sendFile(__dirname + "/portfolio.html")
+})
+
+app.get("/contactus.html", (req, res)=>{
+    res.sendFile(__dirname + "/contactus.html")
+})
+
+app.get("/propertyexpanded.html", (req, res) =>{
+    res.sendFile(__dirname + "/propertyexpanded.html")
+})
+
+app.post("/", (req, res)=>{
+    const fname = req.body.fname;
+    const email = req.body.email;
+    const phoneNumber = req.body.phone;
+    const lname = req.body.lname;
+    console.log(fname);
+})
+
+
 app.listen(3000, function(){
     console.log("this server is running and connected to port 3000");
 });
